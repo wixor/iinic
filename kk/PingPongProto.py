8e9a1c26de911e5fd4f320359b5840ba7e4873b2
Karoluch <karol_konaszynski@wp.pl>
2014-05-23 17:54:05 +0200

    Improved ping pong protocol

--- kk/PingPongProto.py
+++ kk/PingPongProto.py
@@ -11,34 +11,49 @@ class PingPongProto(Proto):
     def __init__(self):
         Proto.__init__(self)
         self.lastReceived = None
+        self.lastPing = None
         
     def onStart(self):
         self.dispatcher.scheduleCallback(self.initPing, time.time()+self.INIT_RETRY+1.0) # listen for some time
     
+    def _constructFrame(self, PingOrPong, nr, t):
+        return '%s %04d %012d' % (PingOrPong, nr, t)
+    
     def initPing(self):
         if not self.lastReceived or time.time()-self.lastReceived > self.DROP_GAME:
-            self.frameLayer.sendFrame(ftype='p', fromId=self.frameLayer.getMyId(), toId=0, content='Ping 1')
-            self.dispatcher.scheduleCallback(self.initPing, time.time() + self.INIT_RETRY + random.random()*0.5) # retry in 2 seconds
+            timing = self.frameLayer.nic.get_approx_timing() + 1000000
+            self.lastPing = self.frameLayer.sendFrame(ftype='p', fromId=self.frameLayer.getMyId(), toId=0, content=self._constructFrame('Ping', 1, timing), timing=timing)
+            self.dispatcher.scheduleCallback(self.initPing, time.time() + self.INIT_RETRY + 0.69) # retry in some time
         
     def handleFrame(self, frame):
-        if frame.fromId() == self.frameLayer.getMyId():
+        if self.lastPing and not self.lastPing.acked:
+            print 'Not sent previous message, ignore'
+            return # ignore this frame
+    
+        if frame.toId() != 0 and frame.toId() != self.frameLayer.getMyId():
             return
         
         received = frame.content()
+        try:
+            nr = int(received[5:9])
+            t = int(received[10:22])
+        except ValueError:
+            print 'Drop frame ', received
+            return
+
         if received[0:4] == 'Ping':
-            reply = 'Pong'
+            what = 'Pong'
         elif received[0:4] == 'Pong':
-            reply = 'Ping'
+            what = 'Ping'
         else:
             return # drop frame
         
-        try:
-            reply += ' ' + str(1 + int(received[5:]))
-        except ValueError:
-            print 'Drop frame ', received
-            return # drop frame
+        # print 'Received: Timing: %012d' % (int(frame.timing())), frame
+        diff = frame.timing()-t if what == 'Ping' else 0
         
+        willSend = frame.timing()+1000000
+        content = self._constructFrame(what, nr+1, willSend-diff)
         # schedule a reply one second after
         self.lastReceived = time.time()
-        self.frameLayer.sendFrame(ftype='p', fromId=self.frameLayer.getMyId(), toId=frame.fromId(), content=reply, timing=frame.timing()+1000000)
+        self.lastPing = self.frameLayer.sendFrame(ftype='p', fromId=self.frameLayer.getMyId(), toId=frame.fromId(), content=content, timing=willSend)
         self.dispatcher.scheduleCallback(self.initPing, time.time()+self.DROP_GAME+0.1)
