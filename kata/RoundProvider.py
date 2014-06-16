fc9ed02299792f2a72d41646cbd4711c00b75ad6
Marcin Januszkiewicz <januszkiewicz.marcin@gmail.com>
2014-06-16 15:53:27 +0200

    First draft of RoundProvider

---
+++ kata/RoundProvider.py
@@ -0,0 +1,13 @@
+class RoundProvider:
+    def __init__(self):
+        pass
+
+    def getRoundNumber(self):
+        pass
+
+    def getRoundDuration(self):
+        pass
+
+    def scheduleFrame(self, frame, roundNumber, forceOffset=0):
+        pass
+    
